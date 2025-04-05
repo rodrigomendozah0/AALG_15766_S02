cc69993585ad20ff86cb8b2e51d595626b00604b
rodrigomendozah0 <rodrigomendozah027@gmail.com>
2025-04-04 19:44:38 -0500

    hello

--- imprimirxd.py
+++ imprimirxd.py
@@ -2,3 +2,5 @@ a = "Rodrigo"
 b = 21
 print("Hola", a, "Mendoza", "tienes", b, "años")
 c = "Hola", str(a), "Mendoza", "tienes", str(b), "años"
+print(c)
+print("Hola", a, "Mendoza", "tienes", b, "años")
