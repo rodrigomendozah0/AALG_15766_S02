bf333acc0f12b5d010c8556e1347c82bcb6f32d6
rodrigomendozah0 <rodrigomendozah027@gmail.com>
2025-04-04 19:40:54 -0500

    first commit

---
+++ Program.py
@@ -0,0 +1,8 @@
+
+a = 2**3
+if a == 8:
+    print("a is 8")
+else:
+    print("a is not 8") 
+
+print("fin")
---
+++ imprimirxd.py
@@ -0,0 +1,4 @@
+a = "Rodrigo"
+b = 21
+print("Hola", a, "Mendoza", "tienes", b, "años")
+c = "Hola", str(a), "Mendoza", "tienes", str(b), "años"
